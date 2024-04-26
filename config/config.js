bee34fcddf238598955f0f4e98dde7ff00508a7a
AbhishekVerma2021 <2001avverma@gmail.com>
2024-04-27 03:00:14 +0530

    adsf

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const port = 5000;
-export const databaseURL = 'mongodb+srv://divyansh:Sharma_123@cluster0.u4bmqvm.mongodb.net/?retryWrites=true&w=majority';
+export const databaseURL = 'mongodb+srv://abhishek-verma:Nis12341234@cluster0.aagekjb.mongodb.net/?retryWrites=true&w=majority';
 export const jwtSecret = "MY_SECRET_KEY";
